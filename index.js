379d68a2318893e088eb1afdf635ffb0ec7b7030
antoninaorobets <pani.antonina.don@gmail.com>
2021-12-08 11:55:37 -0500

    commit tree 0bc7335fd8f43e8fcd64ad90a16aff92f48f53de

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const data = {
+        name: userName,
+        email: userEmail,
+    }
+
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(data)})
+        .then((response) => response.json())
+        .then((respObj) => addIdtoDom(respObj.id))
+        .catch(function (error) {
+            alert("Bad things! Ragnarők!");
+            addErrorToDom(error.message);
+          });
+}
+
+
+function addIdtoDom(id) {
+    const p = document.createElement("p")
+    p.textContent = id;
+    document.querySelector("body").appendChild(p)
+}
+
+
+function addErrorToDom (message){
+    const p = document.createElement("p")
+    p.textContent = message;
+    document.querySelector("body").appendChild(p)
+}
+
+// submitData("Ola", "ola@gmail.com")
